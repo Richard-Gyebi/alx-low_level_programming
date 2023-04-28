9c50ae4d903cbe82fe6b691f9ad7c1c4ce7b78f3
Richard-Gyebi <64223552+Richard-Gyebi@users.noreply.github.com>
2023-04-28 20:28:53 +0000

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef _LISTS_
 #define _LISTS_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,7 +10,9 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
+ * for alx project
  */
 typedef struct list_s
 {
