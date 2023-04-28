bc2ad023e119f42e1f3c16fe971aecfa99933a54
Richard-Gyebi <64223552+Richard-Gyebi@users.noreply.github.com>
2023-04-28 20:43:29 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t nele;
 
-	nelem = 0;
+	nele = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		nele++;
 	}
-	return (nelem);
+	return (nele);
 }
