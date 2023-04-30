29793a9b00c43f316f55c33880c3074fa7768313
Richard-Gyebi <64223552+Richard-Gyebi@users.noreply.github.com>
2023-04-30 18:37:27 +0000

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,12 @@
+
+
 #include "main.h"
 
 /**
- * _puts_recursion - print a string.
- * @s: pointer to char
- * Return: No.
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
@@ -14,7 +15,6 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 	else
-	{
-	_putchar('\n');
-	}
+		_putchar('\n');
 }
+
